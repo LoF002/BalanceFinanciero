13111f2d9bc2e2455cd402c53c833d490945427d
Luis Fernando Espinoza Mora <71098673+LoF002@users.noreply.github.com>
2021-10-27 23:16:46 -0600

    Sensor de huella
    Implementación de métodos, permisos y declaraciones necesarias para utilizar la biometría de huella

--- app/src/main/java/com/example/balancefinanciero/act_login.java
+++ app/src/main/java/com/example/balancefinanciero/act_login.java
@@ -2,6 +2,9 @@ package com.example.balancefinanciero;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,35 +12,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.balancefinanciero.Modelo.Cliente;
-import com.example.balancefinanciero.Modelo.RegistroCliente;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
+import java.util.concurrent.Executor;
 
 public class act_login extends AppCompatActivity {
 
     EditText txtCorreo, txtContrasenia;
     ImageButton btnIngresar, btnRegistrate;
-
-    //Referencias a la clase cliente y registro cliente (Obsoletas por firebase)
-    RegistroCliente registroCliente = new RegistroCliente();
-    ArrayList<Cliente> listaClientes;
-
-    //Variables globales
-    String usuario="", contrasena="";
-    int posicion=0;
-    boolean login;
+    Button btn_biometricLogin;
+    TextView txt_msgHuellaDisponibilidad;
 
     //Referencias a Firebase
     DatabaseReference databaseReference;
@@ -56,9 +50,62 @@ public class act_login extends AppCompatActivity {
         btnIngresar = findViewById(R.id.btnIngresar);
         btnRegistrate = findViewById(R.id.btnRegistrate);
 
+        btn_biometricLogin = findViewById(R.id.btn_biometricLogin);
+        txt_msgHuellaDisponibilidad = findViewById(R.id.txt_msgHuellaDisponibilidad);
+
+
         btnClickListener();
 
+        //creacion del BiometricManager
+        BiometricManager biometricManager = BiometricManager.from(this);
 
+        //Switch para realizar acciones dependiendo de los resultados de la biometria
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                txt_msgHuellaDisponibilidad.setText("Inicio mediante huella digital/biometria disponible");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE: //Esta es para los dispositivos sin lector de huella
+                txt_msgHuellaDisponibilidad.setText("No disponible en este dispositivo");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                txt_msgHuellaDisponibilidad.setText("No disponible en este momento");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                txt_msgHuellaDisponibilidad.setText("No existe registro de una huella");
+                break;
+        }//Fin switch
+
+        Executor executor = ContextCompat.getMainExecutor(getApplicationContext());
+        BiometricPrompt biometricPrompt = new BiometricPrompt(act_login.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override //se llama cuando hay un error con la autenticacion de credenciales
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }//Fin onAuthenticationError
+
+            @Override //se llama cuando se ingresa correctamente
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(act_login.this, "Bienvenido: "+user.getDisplayName(), Toast.LENGTH_SHORT).show();
+            }//Fin onAuthenticationSucceeded
+
+            @Override //se llama cuando falla la autenticacion
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }//Fin onAuthenticationFailed
+        });//Fin AuthenticationCallback
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Ingreso")
+                .setDescription("")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        btn_biometricLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }//Fin onClick
+        });//Fin setOnClickListener
 
 
     }//Fin onCreate
