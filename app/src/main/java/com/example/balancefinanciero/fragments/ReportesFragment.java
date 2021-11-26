5f0e8e71e063937f25b36fa9c5629a69729908ab
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2021-11-25 21:40:04 -0600

    Reportes fixes

--- app/src/main/java/com/example/balancefinanciero/fragments/ReportesFragment.java
+++ app/src/main/java/com/example/balancefinanciero/fragments/ReportesFragment.java
@@ -7,20 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.balancefinanciero.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReportesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ReportesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    //declaracion de los textview
+    TextView txt_numeroMovimientos, txt_numeroCantidadCuentas, txt_numeroGastos, txt_numeroTotalIngresos, txt_numeroDineroEfectivo, txt_numeroTotalDineroActual, txt_numeroTotalCuenta;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -30,14 +30,7 @@ public class ReportesFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReportesFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static ReportesFragment newInstance(String param1, String param2) {
         ReportesFragment fragment = new ReportesFragment();
@@ -61,6 +54,17 @@ public class ReportesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportes, container, false);
+        View vista = inflater.inflate(R.layout.fragment_reportes, container, false);//declaracion de la vista
+        //inicializacion de los text view
+        txt_numeroMovimientos = vista.findViewById(R.id.txt_numeroMovimientos);
+        txt_numeroCantidadCuentas = vista.findViewById(R.id.txt_numeroCantidadCuentas);
+        txt_numeroGastos = vista.findViewById(R.id.txt_numeroGastos);
+        txt_numeroTotalIngresos = vista.findViewById(R.id.txt_numeroTotalIngresos);
+        txt_numeroDineroEfectivo = vista.findViewById(R.id.txt_numeroDineroEfectivo);
+        txt_numeroTotalDineroActual = vista.findViewById(R.id.txt_numeroTotalDineroActual);
+        txt_numeroTotalCuenta = vista.findViewById(R.id.txt_numeroTotalCuenta);
+
+
+        return vista;//se devuelve la vista a usar
     }//fin del onCreateView
 }//fin de la clase
